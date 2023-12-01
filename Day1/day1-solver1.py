89529c34fba082ddf647e39f2ebe7dd2763693e7
Matt Hartfield <matt.hartfield@wwt.com>
2023-12-01 14:25:14 -0600

    Puzzle solution day 1 full

--- Day1/day1-solver1.py
+++ Day1/day1-solver1.py
@@ -8,9 +8,6 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "input")
 
 total = 0
-acceptable_values = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero",
-                     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
 word_to_number = {
     "one": 1,
     "two": 2,
@@ -22,17 +19,45 @@ word_to_number = {
     "eight": 8,
     "nine": 9,
     "zero": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3, 
+    "4": 4, 
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8, 
+    "9": 9, 
+    "0": 0
 }
+
+def find_all_occurrences(word, string):
+    indices = [match.start() for match in re.finditer(word, string)]
+    return indices
+
+def find_matching_values(inputline, acceptable_values):
+    colatedValues = []
+
+    for curSearchValue in acceptable_values:
+        indices = find_all_occurrences(curSearchValue, inputline)
+        if len(indices) > 0:
+            for index in indices:
+                colatedValues.append((curSearchValue, index))
+    
+    return colatedValues
+
+
 
 with open(file_path, 'r') as file:
     for line in file:
-        # from line, find in order, each word in acceptable_values
 
-       # matches = [word for word in acceptable_values if word in line]
-
-        #matches = [word for word in word_to_number for i, char in enumerate(line) if line[i:i+len(word)] == word]
-        numbers = re.findall(r'\d+?', line)
-        calValue = int(numbers[0] + numbers[-1])
+        matching_values = find_matching_values(line, word_to_number.keys())
+        sorted_values = sorted(matching_values, key=lambda x: x[1])
+        numeric_values = [word_to_number[value[0]] for value in sorted_values]
+        
+        joined_two_digit = str(numeric_values[0]) + str(numeric_values[-1])
+        calValue = int(joined_two_digit)
+        
         total += calValue
        
 
