429d4b598b5de091effc77b0a09539ce1d0ceed0
Matt Hartfield <matt.hartfield@wwt.com>
2023-12-01 08:23:14 -0600

    Puzzle solution to Day 1.1

---
+++ Day1/day1-solver1.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+# Get the path of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the file you want to open
+file_path = os.path.join(current_dir, "input")
+
+total = 0
+
+with open(file_path, 'r') as file:
+    for line in file:
+        # Parse each line here
+        numbers = re.findall(r'\d+?', line)
+        calValue = int(numbers[0] + numbers[-1])
+        total += calValue
+       
+
+print(total)
