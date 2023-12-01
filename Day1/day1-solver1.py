1d95b7be78e291484e060e2509f593155270a9b9
Matt Hartfield <matt.hartfield@wwt.com>
2023-12-01 08:53:13 -0600

    Start on solving part day 1 part 2

--- Day1/day1-solver1.py
+++ Day1/day1-solver1.py
@@ -8,10 +8,29 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "input")
 
 total = 0
+acceptable_values = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero",
+                     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+word_to_number = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
+}
 
 with open(file_path, 'r') as file:
     for line in file:
-        # Parse each line here
+        # from line, find in order, each word in acceptable_values
+
+       # matches = [word for word in acceptable_values if word in line]
+
+        #matches = [word for word in word_to_number for i, char in enumerate(line) if line[i:i+len(word)] == word]
         numbers = re.findall(r'\d+?', line)
         calValue = int(numbers[0] + numbers[-1])
         total += calValue
