f42dcd27c5ff9645110bd018986aae6f50a2cdef
Matt Hartfield <hartfield@gmail.com>
2023-12-03 09:53:47 -0600

    Day3 Now collects numbers in line dict

--- Day3/day3_solver.py
+++ Day3/day3_solver.py
@@ -21,9 +21,12 @@ class NumberEntry:
 class SymbolEntry:
     Symbol = ""
     Index = 0
+    IsGear = False
+    PossibleGear = False
+    GearNumbers = []
 
-NumberEntrys = []
-SymbolEntrys = {}
+NumberEntrysByLine = {}
+SymbolEntrysByLine = {}
 
 def build(file_path):    
     with open(file_path, 'r') as file:
@@ -32,11 +35,10 @@ def build(file_path):
             locations = []
             for num in numbers:
                 locations.append(line.index(num))
-
                 #update line to replace num with spaces
                 line = line.replace(num, ' '*len(num), 1)
-                
 
+            numberEntrys = []
             for number, loc in zip(numbers, locations):
                 numberEntry = NumberEntry()
                 numberEntry.Line = idx
@@ -45,7 +47,9 @@ def build(file_path):
                 numberEntry.Start = loc
                 numberEntry.End = loc + len(number)-1
                 numberEntry.Include = False
-                NumberEntrys.append(numberEntry)
+                numberEntrys.append(numberEntry)
+
+            NumberEntrysByLine[idx] = numberEntrys
 
             symbols = re.finditer(r'[&\+\-#\@\$*/%=]', line)
             symbolEntrys = []
@@ -53,38 +57,41 @@ def build(file_path):
                 symbolEntry = SymbolEntry()
                 symbolEntry.Index = symbol.start()
                 symbolEntry.Symbol = symbol.group()
+                if (symbolEntry.Symbol == "*"):
+                    symbolEntry.PossibleGear = True
                 symbolEntrys.append(symbolEntry)
            
-            SymbolEntrys[idx] = symbolEntrys  
+            SymbolEntrysByLine[idx] = symbolEntrys  
 
         
 def find_includes():
-    for number in NumberEntrys:
-        #Check Above
-        if number.Line > 0:
-            line_above = number.Line - 1
-            include_overlap(number, line_above)
+    for line in NumberEntrysByLine:
+        for number in NumberEntrysByLine[line]:
+            #Check Above
+            if number.Line > 0:
+                line_above = number.Line - 1
+                include_overlap(number, line_above)
 
-        if(number.Include):
-            continue
-        
-        #Check Left and Right
-        for symbolEntry in SymbolEntrys[number.Line]:
-            if (symbolEntry.Index == number.Start-1) or (symbolEntry.Index == number.End+1):
-                number.Include = True
-                break
-        
-        if(number.Include):
-            continue      
+            if(number.Include):
+                continue
+            
+            #Check Left and Right
+            for symbolEntry in SymbolEntrysByLine[number.Line]:
+                if (symbolEntry.Index == number.Start-1) or (symbolEntry.Index == number.End+1):
+                    number.Include = True
+                    break
+            
+            if(number.Include):
+                continue      
 
-        #Check Below
-        if number.Line < len(SymbolEntrys) - 1:
-            line_below = number.Line + 1
-            include_overlap(number, line_below)
+            #Check Below
+            if number.Line < len(SymbolEntrysByLine) - 1:
+                line_below = number.Line + 1
+                include_overlap(number, line_below)
 
 
 def include_overlap(number, line_number):
-    for symbol in SymbolEntrys[line_number]:
+    for symbol in SymbolEntrysByLine[line_number]:
         #if symbol is in the range of number.start-1 to number.end+1 then include
         if (symbol.Index >= number.Start-1) and (symbol.Index <= number.End+1):
             number.Include = True
@@ -93,11 +100,33 @@ def include_overlap(number, line_number):
 
 def sum_includes():
     sum = 0
-    for number in NumberEntrys:
-        if number.Include:
-            sum += number.Number
+    for line in NumberEntrysByLine:
+        for number in NumberEntrysByLine[line]:
+            if number.Include:
+                sum += number.Number
     print(sum)
 
+
+def find_gears():
+    #for each symbol entry, if PossibleGear
+    #check the line above, on, and below
+    #for numbers that touch the symbol
+    #if there are exactly 2 numbers this is a gear
+    for line in SymbolEntrysByLine:
+        for symbolEntry in SymbolEntrysByLine[line]:
+            if not symbolEntry.PossibleGear:
+                continue
+            
+
+
+
+
+
 build(file_path)
+
+#Part 1
 find_includes()
 sum_includes()
+
+#Part 2
+find_gears()
