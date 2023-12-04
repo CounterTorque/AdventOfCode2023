e10a9e19a325c5f57484c5db0980e27d5b8aaf71
Matt Hartfield <hartfield@gmail.com>
2023-12-04 08:06:07 -0600

    minor refactors to day 4

--- Day4/day4_solver.py
+++ Day4/day4_solver.py
@@ -5,16 +5,20 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "input.txt")
 
 class ScratchCard:
-    CardNumber = 0
-    Copies = 1
-    WinningNumbers = []
-    ShownNumbers = []
-    CountMatches = 0
-    CardPoints = 0
+    def __init__(self, card_number, winning_numbers, shown_numbers):
+        self.CardNumber = card_number
+        self.Copies = 1
+        self.WinningNumbers = winning_numbers
+        self.ShownNumbers = shown_numbers
+        self.CountMatches = 0
+        self.CardPoints = 0
 
-scratch_cards = []
 
-def build(file_path):    
+
+
+def build(file_path): 
+    scratch_cards = []
+
     with open(file_path, 'r') as file:
         for idx, line in enumerate(file):
             _, number_sets = line.strip().split(':')
@@ -22,15 +26,17 @@ def build(file_path):
             winning_numbers = winning_numbers.strip()
             shown_numbers = shown_numbers.strip()
 
-            scratch_card = ScratchCard()
-            scratch_card.CardNumber = idx
-            scratch_card.WinningNumbers = re.findall(r'\d+', winning_numbers)
-            scratch_card.ShownNumbers = re.findall(r'\d+', shown_numbers)
-
+            scratch_card = ScratchCard(idx, 
+                                       re.findall(r'\d+', winning_numbers), 
+                                       re.findall(r'\d+', shown_numbers)
+            )
+           
             scratch_cards.append(scratch_card)
+    
+    return scratch_cards
 
 
-def check_cards_for_matches(scractch_cards):
+def check_cards_for_matches(scratch_cards):
     for scratch_card in scratch_cards:
         for shown_number in scratch_card.ShownNumbers:
             if shown_number in scratch_card.WinningNumbers:
@@ -40,16 +46,16 @@ def check_cards_for_matches(scractch_cards):
 def generate_points_and_copies(scratch_cards):
     total_score = 0
     for scratch_card in scratch_cards:
-        if (scratch_card.CountMatches == 0):
+        if scratch_card.CountMatches == 0:
             continue
 
-        scratch_card.CardPoints = 2**(scratch_card.CountMatches-1)
+        scratch_card.CardPoints = 2 ** (scratch_card.CountMatches-1)
         total_score += scratch_card.CardPoints
 
         copy_count = scratch_card.Copies
         for i in range(scratch_card.CountMatches):
             next_card_number = scratch_card.CardNumber + i + 1
-            if (next_card_number >= len(scratch_cards)):
+            if next_card_number >= len(scratch_cards):
                 break
             scratch_cards[next_card_number].Copies += copy_count
 
@@ -64,7 +70,7 @@ def count_copies(scratch_cards):
     print(total_copies)
     #5554894 Part 2
 
-build(file_path)
+scratch_cards = build(file_path)
 check_cards_for_matches(scratch_cards)
 generate_points_and_copies(scratch_cards)
 count_copies(scratch_cards)
