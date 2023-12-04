282abac041a3cdeac70d555a6303a3a90a217dc2
Matt Hartfield <hartfield@gmail.com>
2023-12-04 08:00:05 -0600

    Day 4 part 2

--- Day4/day4_solver.py
+++ Day4/day4_solver.py
@@ -6,9 +6,11 @@ file_path = os.path.join(base_dir, "input.txt")
 
 class ScratchCard:
     CardNumber = 0
+    Copies = 1
     WinningNumbers = []
     ShownNumbers = []
     CountMatches = 0
+    CardPoints = 0
 
 scratch_cards = []
 
@@ -35,20 +37,35 @@ def check_cards_for_matches(scractch_cards):
                 scratch_card.CountMatches += 1
 
 
-def generate_scores(scratch_cards):
+def generate_points_and_copies(scratch_cards):
     total_score = 0
     for scratch_card in scratch_cards:
         if (scratch_card.CountMatches == 0):
             continue
 
-        card_score = 2**(scratch_card.CountMatches-1)
-        total_score += card_score
+        scratch_card.CardPoints = 2**(scratch_card.CountMatches-1)
+        total_score += scratch_card.CardPoints
+
+        copy_count = scratch_card.Copies
+        for i in range(scratch_card.CountMatches):
+            next_card_number = scratch_card.CardNumber + i + 1
+            if (next_card_number >= len(scratch_cards)):
+                break
+            scratch_cards[next_card_number].Copies += copy_count
 
     print(total_score)
+    #17803 Part 1
 
+
+def count_copies(scratch_cards):
+    total_copies = 0
+    for scratch_card in scratch_cards:
+        total_copies += scratch_card.Copies
+    print(total_copies)
+    #5554894 Part 2
 
 build(file_path)
 check_cards_for_matches(scratch_cards)
-generate_scores(scratch_cards)
-
+generate_points_and_copies(scratch_cards)
+count_copies(scratch_cards)
 
