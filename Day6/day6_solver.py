96063e83cdd83158b63168585e5bbe6eb286a5a4
Matt Hartfield <hartfield@gmail.com>
2023-12-05 19:06:55 -0600

    setup boilerplate for day6

---
+++ Day6/day6_solver.py
@@ -0,0 +1,13 @@
+import os
+import re
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, "input.txt")
+
+def extract_data(file_path): 
+    with open(file_path, 'r') as file:
+        for idx, line in enumerate(file):
+            return
+        
+extract_data(file_path)
+
