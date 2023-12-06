cbb3be485bed67f6633c1ef45a76df380185136c
Matt Hartfield <hartfield@gmail.com>
2023-12-05 19:03:58 -0600

    wrote a reverse search for part 2

--- Day5/day5_solverv2.py
+++ Day5/day5_solverv2.py
@@ -15,22 +15,18 @@ key_names = ["seeds:",
 
 
 class SeedData:
-    SeedNumber = 0
-    Soil = 0
-    Fertilizer = 0
-    Water = 0
-    Light = 0
-    Temperature = 0
-    Humidity = 0
-    Location = 0
+    def __init__(self, seed_start, range):
+        self.SeedStart = seed_start
+        self.SeedEnd = seed_start + range
 
 
 class LookupMap:
     def __init__(self, category, destination, source, range_length):
         self.Category = category
         self.Destination = destination
+        self.DestinationEnd = destination + range_length
         self.Source = source
-        self.RangeLength = range_length
+        self.SourceEnd = source + range_length
 
 
 def init_seeds(seed_set, seed_strings):
@@ -44,10 +40,8 @@ def init_seeds(seed_set, seed_strings):
         
         is_start = True
         seed_range = int(seed)
-        for seed_num in range(seed_start, seed_range + seed_start):
-            next_seed = SeedData()
-            next_seed.SeedNumber = seed_num
-            seed_set.append(next_seed)
+        next_seed = SeedData(seed_start, seed_range)
+        seed_set.append(next_seed)
 
 
 def extract_data(file_path): 
@@ -75,38 +69,53 @@ def extract_data(file_path):
     
     return seed_set, lookup_maps
 
-        
-def build_full_seed(seed_set, lookup_maps):
-    for seed in seed_set:
-        seed.Soil = find_item(seed.SeedNumber, lookup_maps["seed-to-soil map:"])
-        seed.Fertilizer = find_item(seed.Soil, lookup_maps["soil-to-fertilizer map:"])
-        seed.Water = find_item(seed.Fertilizer, lookup_maps["fertilizer-to-water map:"])
-        seed.Light = find_item(seed.Water, lookup_maps["water-to-light map:"])
-        seed.Temperature = find_item(seed.Light, lookup_maps["light-to-temperature map:"])
-        seed.Humidity = find_item(seed.Temperature, lookup_maps["temperature-to-humidity map:"])
-        seed.Location = find_item(seed.Humidity, lookup_maps["humidity-to-location map:"])
-       
 
-def find_map_index(search, lookup_map):
-    if (lookup_map.Source <= search) and (search < lookup_map.Source + lookup_map.RangeLength):
-        offset = search - lookup_map.Source
-        return lookup_map.Destination + offset
+def check_seed_exists(seed_search, seed_set):
+    for seed in seed_set:
+        if (seed.SeedStart <= seed_search) and (seed_search < seed.SeedEnd):
+            return True
+    return False
+
+
+def find_lowest_location(seed_set, lookup_maps):
+    lowest_location = 0
+    while True:   
+        cur_Humidity = find_item(lowest_location, lookup_maps["humidity-to-location map:"])        
+        cur_Temperature = find_item(cur_Humidity, lookup_maps["temperature-to-humidity map:"])        
+        cur_Light = find_item(cur_Temperature, lookup_maps["light-to-temperature map:"])        
+        cur_Water = find_item(cur_Light, lookup_maps["water-to-light map:"])        
+        cur_Fertilizer = find_item(cur_Water, lookup_maps["fertilizer-to-water map:"])
+        cur_Soil = find_item(cur_Fertilizer, lookup_maps["soil-to-fertilizer map:"])
+        cur_Seed = find_item(cur_Soil, lookup_maps["seed-to-soil map:"])
+
+        # find if seed exists for soil
+        if check_seed_exists(cur_Seed, seed_set):
+            return lowest_location
+        
+        lowest_location += 1
+        
+
+def sort_maps(lookup_maps):
+    for item in lookup_maps:
+        lookup_maps[item].sort(key=lambda x: x.Destination)
+       
+def find_source_from_destination(search, lookup_map):
+    if (lookup_map.Destination <= search) and (search < lookup_map.DestinationEnd):
+        offset = search - lookup_map.Destination
+        return lookup_map.Source + offset
     
     return -1
 
 
 def find_item(search_number, lookup_maps):
     for lookup_map in lookup_maps:
-        index = find_map_index(search_number, lookup_map)
+        index = find_source_from_destination(search_number, lookup_map)
         if (index != -1):
             return index
         
     return search_number
 
-
 seed_set, lookup_maps = extract_data(file_path)
-build_full_seed(seed_set, lookup_maps)
-# Part 1 (pre split 199602917)
-#This solves part 2 only
-lowest_location = min(seed.Location for seed in seed_set)
-print(lowest_location)
+sort_maps(lookup_maps)
+lowest_location = find_lowest_location(seed_set, lookup_maps)
+print(lowest_location) #2254686
