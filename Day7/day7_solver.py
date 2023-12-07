c8159792fa0f309a9f468f4468d2f9df8f76324b
Matt Hartfield <hartfield@gmail.com>
2023-12-07 13:06:43 -0600

    Partial implementation for wilds on day 7

--- Day7/day7_solver.py
+++ Day7/day7_solver.py
@@ -1,10 +1,16 @@
 import os
 import re
+from collections import Counter
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "input.txt")
 
-card_names = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+#part 1
+#card_names = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+#part 2
+card_names = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+
 FIVE_OF_A_KIND = 6
 FOUR_OF_A_KIND = 5
 FULL_HOUSE = 4
@@ -12,6 +18,11 @@ THREE_OF_A_KIND = 3
 TWO_PAIR = 2
 ONE_PAIR = 1
 HIGH_CARD = 0
+
+def most_common_character(string):
+    counter = Counter(string)
+    most_common = counter.most_common(1)
+    return most_common[0][0] if most_common else None
 
 
 class Hand:
@@ -25,30 +36,52 @@ class Hand:
     def parse_cards(self, cards):
         return [card_names.index(card) for card in cards]
     
+    def wild_hand(self):
+        if not re.search(r'J', self.Cards):
+            return self.Cards, self.Hand
+        
+        #part 2 only
+         # find highest count of any other card (if tie use highest value)
+        #sort the cards by value first, so we can just use the top hit to solve ties
+        cards_sorted = self.Cards #TODO
+        
+        best_card_letter = most_common_character(cards_sorted)
+        best_card_num = card_names.index(best_card_letter)
+        
+
+        card_adj = ""
+        hand_adj = []
+        for card_id in self.Hand:
+            if card_id == card_names.index("J"):
+                card_adj += best_card_letter
+                hand_adj.append(best_card_num)
+            else:
+                card_adj += card_names[card_id]
+                hand_adj.append(card_id)
+
+
+        return card_adj, hand_adj
    
     def calculate_rank(self):
-        # To handle jokers
-        # find highest count of any other card
-        # replace jokers with that card
-        # if there is a tie for the highest count of any other card, use the highest individual value
+        cards_adj, hand_adj = self.wild_hand()
 
 
 
-        distinct_cards = set(self.Hand)
+        distinct_cards = set(hand_adj)
         if (len(distinct_cards) == 1): 
             return FIVE_OF_A_KIND
        
         if (len(distinct_cards) == 2):
-            count_card1 = len(re.findall(re.escape(self.Cards[0]), self.Cards))
+            count_card1 = len(re.findall(re.escape(cards_adj[0]), cards_adj))
             if (count_card1 == 4 or count_card1 == 1):
                 return FOUR_OF_A_KIND
                         
             return FULL_HOUSE
 
         if (len(distinct_cards) == 3):
-            count_card1 = len(re.findall(re.escape(self.Cards[0]), self.Cards))
-            count_card2 = len(re.findall(re.escape(self.Cards[1]), self.Cards))
-            count_card3 = len(re.findall(re.escape(self.Cards[3]), self.Cards))
+            count_card1 = len(re.findall(re.escape(cards_adj[0]), cards_adj))
+            count_card2 = len(re.findall(re.escape(cards_adj[1]), cards_adj))
+            count_card3 = len(re.findall(re.escape(cards_adj[3]), cards_adj))
             if (count_card1 == 3 or count_card2 == 3 or count_card3 == 3):
                 return THREE_OF_A_KIND
 
