bbe2d75671b35f8bd97b612f51b5f05256a5525c
Matt Hartfield <hartfield@gmail.com>
2023-12-08 07:41:06 -0600

    day 8 part 1 solution

--- Day8/day8_solver.py
+++ Day8/day8_solver.py
@@ -3,11 +3,59 @@ import os
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "input.txt")
 
+cur_instruction = 0
+cur_step = 0
+
+class Element:
+    def __init__(self, name, left, right):
+        self.name = name.strip()
+        self.left = left.strip()
+        self.right = right.strip()
+
 
 def extract_data(file_path):
+    elements = {}
+    instructions = []
+    first_elem = ""
     with open(file_path, 'r') as file:
         for line in file:
-            return
+            line = line.strip()
+            if len(instructions) == 0:                
+                instructions = [char for char in line]
+                continue
+            elif line == "":
+                continue
+            else:
+                key, nodes = line.split(" = ")
+                left, right = nodes.split(",")
+                elements[key] = Element(key, left.strip("("), right.rstrip(")"))
+                if first_elem == "":
+                    first_elem = key
+            
+    return first_elem, elements, instructions
         
 
-extract_data(file_path)
+def walk_instructions(first_elem, elements, instructions):
+    cur_instruction = 0
+    cur_step = 0
+    cur_node = "AAA"
+
+    while cur_node != "ZZZ":
+        if cur_instruction == len(instructions):
+            cur_instruction = 0
+
+        next_dir = instructions[cur_instruction]
+        if next_dir == "R":
+            cur_node = elements[cur_node].right
+        elif next_dir == "L":
+            cur_node = elements[cur_node].left
+        cur_instruction += 1
+        cur_step += 1
+
+    return cur_step
+
+
+first_elem, elements, instructions = extract_data(file_path)
+total_steps = walk_instructions(first_elem, elements, instructions)
+
+print(total_steps) #16343 Part 1
