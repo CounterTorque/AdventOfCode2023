614d0ea138c1177ccc7cabbdef6c4108c6b2a74b
Matt Hartfield <hartfield@gmail.com>
2023-12-08 19:07:20 -0600

    Added base solver with input read

---
+++ incomplete/Day24/day24_solver.py
@@ -0,0 +1,13 @@
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, "input.txt")
+
+def extract_data(file_path):
+    with open(file_path, 'r') as file:
+         for line in file:
+              return
+         
+extract_data(file_path)
+
+
