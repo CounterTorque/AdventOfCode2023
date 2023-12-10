23ed2a96cb57a08a11b6f28f5592d09a4b92de62
Matt Hartfield <hartfield@gmail.com>
2023-12-10 12:27:47 -0600

    refactor to day 10 to cleaner code

--- Day10/day10_solver.py
+++ Day10/day10_solver.py
@@ -14,22 +14,19 @@ class PipeSegment:
           self.step_number = EMPTY
 
      def connections(self):
-          if self.type == "|":
-              return (self.y + 1, self.x), (self.y - 1, self.x)
-          elif self.type == "-":
-               return (self.y, self.x + 1), (self.y, self.x - 1)
-          elif self.type == "L":
-               return (self.y, self.x + 1), (self.y - 1, self.x)
-          elif self.type == "J":
-               return (self.y, self.x - 1), (self.y - 1, self.x)
-          elif self.type == "7":
-               return (self.y + 1, self.x), (self.y, self.x - 1)
-          elif self.type == "F":
-               return (self.y, self.x + 1), (self.y + 1, self.x)
-          elif self.type == ".":
-               return None, None
-          elif self.type == "S":
-               return None, None
+          type_mapping = {
+               "|": ((self.y + 1, self.x), (self.y - 1, self.x)),
+               "-": ((self.y, self.x + 1), (self.y, self.x - 1)),
+               "L": ((self.y, self.x + 1), (self.y - 1, self.x)),
+               "J": ((self.y, self.x - 1), (self.y - 1, self.x)),
+               "7": ((self.y + 1, self.x), (self.y, self.x - 1)),
+               "F": ((self.y, self.x + 1), (self.y + 1, self.x)),
+               ".": (None, None),
+               "S": (None, None)
+          }
+          
+          return type_mapping.get(self.type)
+ 
      
      def exit_connection(self, input_y, input_x):
           conn_a, conn_b = self.connections()
@@ -42,35 +39,35 @@ class PipeSegment:
            
 
 
-def start_connections(pipe_segement: PipeSegment, pipe_map:[[]]):
-          assert(pipe_segement.type == "S")
+def start_connections(pipe_segment: PipeSegment, pipe_map:[[]]):
+          assert(pipe_segment.type == "S")
 
-          up = pipe_segement.y - 1
-          down = pipe_segement.y + 1
-          left = pipe_segement.x - 1
-          right = pipe_segement.x + 1
+          up = pipe_segment.y - 1
+          down = pipe_segment.y + 1
+          left = pipe_segment.x - 1
+          right = pipe_segment.x + 1
 
           connections = []
 
           if up >= 0:
-               pipe_check = pipe_map[up][pipe_segement.x]
-               if pipe_check.type == "|" or pipe_check.type == "7" or pipe_check.type == "F":
-                    connections.append((up, pipe_segement.x))
+               pipe_check = pipe_map[up][pipe_segment.x]
+               if pipe_check.type in ["|", "7", "F"]:
+                    connections.append((up, pipe_segment.x))
           
           if down < len(pipe_map):
-               pipe_check = pipe_map[down][pipe_segement.x]
-               if pipe_check.type == "|" or pipe_check.type == "L" or pipe_check.type == "J":
-                    connections.append((down, pipe_segement.x))
+               pipe_check = pipe_map[down][pipe_segment.x]
+               if pipe_check.type in ["|", "L", "J"]:
+                    connections.append((down, pipe_segment.x))
 
           if left >= 0:
-               pipe_check = pipe_map[pipe_segement.y][left]
-               if pipe_check.type == "-" or pipe_check.type == "L" or pipe_check.type == "F":
-                    connections.append((pipe_segement.y, left))
+               pipe_check = pipe_map[pipe_segment.y][left]
+               if pipe_check.type in ["-", "L", "F"]:
+                    connections.append((pipe_segment.y, left))
 
           if right < len(pipe_map[0]):
-               pipe_check = pipe_map[pipe_segement.y][right]
-               if pipe_check.type == "-" or pipe_check.type == "J" or pipe_check.type == "7":
-                    connections.append((pipe_segement.y, right))
+               pipe_check = pipe_map[pipe_segment.y][right]
+               if pipe_check.type in ["-", "J", "7"]:
+                    connections.append((pipe_segment.y, right))
 
           assert(len(connections) == 2)
           return connections[0], connections[1]
@@ -78,13 +75,10 @@ def start_connections(pipe_segement: PipeSegment, pipe_map:[[]]):
                
           
 def extract_data(file_path: str):
-     x = 0
-     y = 0
      pipe_map = []
      start_x = 0
      start_y = 0
      with open(file_path, 'r') as file:
-          #I don't know the right way to work with a 2d list in python, so we are doing this
           lines = file.readlines()
           char_list = [list(line.strip()) for line in lines]
 
@@ -103,7 +97,7 @@ def extract_data(file_path: str):
      return pipe_map, start_y, start_x 
 
 
-def walk_pipes(pipe_map:[[]], start_y, start_x):
+def walk_pipes(pipe_map:[[]], start_y:int , start_x:int ) -> int:
      pipe_segement = pipe_map[start_y][start_x]
      
      cur_a = (start_y, start_x)
@@ -116,7 +110,6 @@ def walk_pipes(pipe_map:[[]], start_y, start_x):
           pipe_b = pipe_map[next_b[0]][next_b[1]]
           pipe_a.step_number = "A" + str(steps).rjust(4, "0") + " "
           pipe_b.step_number = "B" + str(steps).rjust(4, "0") + " "
-          #print(pipe_b.type + " ", end="")
           steps += 1
 
           next_a = pipe_a.exit_connection(cur_a[0], cur_a[1])
@@ -152,23 +145,21 @@ def flood_fill_outer(pipe_map = [[]]):
 
 pipe_map, start_y, start_x = extract_data(file_path)
 farthest_step = walk_pipes(pipe_map, start_y, start_x )
-print("Part 1: ",farthest_step)
+print("Part 1: ",farthest_step) #6820
 
 flood_fill_outer(pipe_map)
 #debug print map
 
-output_path = os.path.join(base_dir, "output.txt")
-output_file = open(output_path, 'w')
-output_file.truncate(0)
+def print_output_map(base_dir, pipe_map):
+     output_path = os.path.join(base_dir, "output.txt")
+     with open(output_path, 'w') as output_file:
+          for row in pipe_map:
+               for pipe_segment in row:
+                    output_file.write(pipe_segment.step_number)
+                    output_file.flush()
+               output_file.write('\n')
 
-for y in range(len(pipe_map)):
-     for x in range(len(pipe_map[0])):
-          pipe_segement = pipe_map[y][x]
-          output_file.write(pipe_segement.step_number)
-          output_file.flush()
-     output_file.write('\n')
-
-output_file.close()
+print_output_map(base_dir, pipe_map)
 
 #part 2 
 # 201 is too low
