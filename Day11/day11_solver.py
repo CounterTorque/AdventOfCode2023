38a1d555d7e1ab81274d4880ede2960eca732290
Matt Hartfield <hartfield@gmail.com>
2023-12-11 07:49:16 -0600

    day 11 part 1. Needs complete refactor for part 2

--- Day11/day11_solver.py
+++ Day11/day11_solver.py
@@ -20,21 +20,58 @@ def extract_data(file_path: str) -> [[]]:
      return galaxy_map
 
 def expand_map(galaxy_map: [[]]) -> [[]]:
-     #todo for each line and column that is only '.' expand the map down or right
-     return
+     
+     y = 0
+     while True:
+          has_y_space = all(item == "." for item in galaxy_map[y])
+          if has_y_space:
+               galaxy_map.insert(y + 1, ["."] * len(galaxy_map[0]))
+               y += 1
+          
+          y += 1
+          if (y >= len(galaxy_map)):
+               break
+
+
+     x = 0
+     while True:
+          has_x_space = all(item == "." for item in [row[x] for row in galaxy_map])
+          if has_x_space:
+               for row in galaxy_map:
+                    row.insert(x + 1, ".")
+               x += 1
+          
+          x += 1
+          if (x >= len(galaxy_map[0])):
+               break
+               
+     return galaxy_map
 
 def find_locations(galaxy_map: [[]]) -> [()]:
-     #todo find each non '.' and return a tuple of its (y, x) location
-     return
+     locations = []
+     for y in range(len(galaxy_map)):
+          for x in range(len(galaxy_map[y])):
+               if galaxy_map[y][x] != ".":
+                    locations.append((y, x))
+
+     return locations
 
 def find_distance(galaxy_locations: [()], galaxy_map: [[]]) -> int:
-     return
+     distance_total = 0
+     for index_location in range(len(galaxy_locations)):
+          source_location = galaxy_locations[index_location]
+          for dest_location in galaxy_locations[index_location + 1:]:
+               distance = abs(source_location[0] - dest_location[0]) + abs(source_location[1] - dest_location[1])
+               distance_total += distance
+          
+
+     return distance_total
          
 galaxy_map = extract_data(file_path)
 galaxy_map = expand_map(galaxy_map)
 galaxy_locations = find_locations(galaxy_map)
 total_distance = find_distance(galaxy_locations, galaxy_map)
 
-print("Part 1: " + str(total_distance))
+print("Part 1: " + str(total_distance)) # 10231178
 
 
