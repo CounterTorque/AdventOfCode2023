6225b953a83ff0d8554f0b2a7f0432e5b9b1cd0e
Matt Hartfield <hartfield@gmail.com>
2023-12-12 17:27:01 -0600

    day 12 - temp commit to try anothe approach

--- Day12/day12_solver.py
+++ Day12/day12_solver.py
@@ -1,7 +1,7 @@
 import os
 import re
-import math
 import itertools
+import cProfile
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "input.txt")
@@ -11,10 +11,9 @@ class SpringRow:
      def __init__(self, row:str):
           records, specifiers = row.split(' ')
           self.records = records.strip()
-          self.specifiers = specifiers.strip()
 
-          self.spec_list = [int(x) for x in re.findall(r'\d+', self.specifiers)]
-          self.solutions = []
+          self.spec_list = [int(x) for x in re.findall(r'\d+', specifiers)]
+          self.total_solutions = 0
 
 
 def extract_data(file_path:str) -> []:
@@ -26,64 +25,88 @@ def extract_data(file_path:str) -> []:
 
      return spring_data
 
+def unfold_records(spring_data: SpringRow):
+     for spring in spring_data: 
+          original_record = spring.records
+          for _ in range(4):
+               spring.records += "?" + original_record
 
-def generate_permutations(spring_data: SpringRow)-> [str]:
-     #for every ? in the row, generate a version with it replaced with "#" or "."
-     built_permutations = []
-     options = ['#', '.']
-     permutations = itertools.product(options, repeat=spring_data.records.count('?'))
+          spring.spec_list = spring.spec_list * 5
 
-     for permutation in permutations:
-          result = spring_data.records
-          for replacement in permutation:
-               result = result.replace('?', replacement, 1)
-          built_permutations.append(result)
-          
-     return built_permutations
 
 def is_valid_solution(solution: str, spec_list:[int]) -> bool:
-     # go ahead and move the solution forward by the '.'s at the head
+     # Remove leading dots from the solution
      solution = solution.lstrip('.')
+
+     # Iterate over each specifier in spec_list
      for spec in spec_list:
           expected_string = "#" * spec
+          # Check if the solution starts with the expected string
           if not solution.startswith(expected_string):
                return False
           
+          # Remove the consumed portion from the solution
           solution = solution[spec:]
-          if len(solution) > 0:
-               if (solution[0] != '.'):
-                    return False
+
+          # Check if there are remaining characters in the solution
+          if solution and solution[0] != '.':
+               return False
           
           solution = solution.lstrip('.')
      
-     #at this point we have consumed all the specifiers. The rest of the solution must be all '.'
-     if len(solution) > 0:        
+
+     if solution:
           return False
 
      return True
 
 
 def find_solutions(spring_data):
-     for row in spring_data:
-          permutations = generate_permutations(row)
-          for solution in permutations:
-               if is_valid_solution(solution, row.spec_list):
-                    row.solutions.append(solution)
+     options = ['#', '.']
 
+     for row in spring_data:
+          #simplify the data
+          record_check = row.records
+          max_spec = sum(row.spec_list)
+
+          permutations = itertools.product(options, repeat=record_check.count('?'))
+
+          for permutation in permutations:
+               result = record_check
+               for replacement in permutation:
+                    result = result.replace('?', replacement, 1)
+                    
+               # precheck
+               if result.count('#') != max_spec:
+                    continue
+
+               if is_valid_solution(result, row.spec_list):
+                    row.total_solutions += 1
+
+
+          print(row.total_solutions)
 
 
 def total_solutions(spring_data):
      total = 0
      for row in spring_data:
-          total += len(row.solutions)
+          total += row.total_solutions
 
      return total
 
+profiler = cProfile.Profile()
+profiler.enable()
 
 spring_data = extract_data(file_path)
+#Part 2 Only
+unfold_records(spring_data)
+
 find_solutions(spring_data)
 total_solutions = total_solutions(spring_data)
 
-print(f"Part 1: {total_solutions}") #7541
+print(f"total solutions: {total_solutions}") #7541
+profiler.disable()
+profiler.print_stats()
 
+#12303440 function calls (12303436 primitive calls) in 3.344 seconds
 
