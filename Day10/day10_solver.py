87a7d294814310c266014c8b41a168611bebd40b
Matt Hartfield <hartfield@gmail.com>
2023-12-13 17:22:17 -0600

    Day 10 part 2 solution

--- Day10/day10_solver.py
+++ Day10/day10_solver.py
@@ -6,12 +6,40 @@ file_path = os.path.join(base_dir, "input.txt")
 EMPTY = "((.)) "
 OUTSIDE = "      "
 
+matrix_map = {
+     "|":[[' ','X',' '],
+          [' ','X',' '],
+          [' ','X',' ']],
+     "-":[[' ',' ',' '],
+          ['X','X','X'],
+          [' ',' ',' ']],
+     "L":[[' ','X',' '],
+          [' ','X','X'],
+          [' ',' ',' ']],
+     "J":[[' ','X',' '],
+          ['X','X',' '],
+          [' ',' ',' ']],
+     "7":[[' ',' ',' '],
+          ['X','X',' '],
+          [' ','X',' ']],
+     "F":[[' ',' ',' '],
+          [' ','X','X'],
+          [' ','X',' ']],
+     ".":[['O','O','O'],
+          ['O','O','O'],
+          ['O','O','O']],
+     "S":[[' ',' ',' '],
+          [' ','X',' '],
+          [' ',' ',' ']],
+}
+
 class PipeSegment:
      def __init__(self, y, x, type):
           self.y = y
           self.x = x
           self.type = type
           self.step_number = EMPTY
+          self.matrix = matrix_map[type]
 
      def connections(self):
           type_mapping = {
@@ -53,21 +81,26 @@ def start_connections(pipe_segment: PipeSegment, pipe_map:[[]]):
                pipe_check = pipe_map[up][pipe_segment.x]
                if pipe_check.type in ["|", "7", "F"]:
                     connections.append((up, pipe_segment.x))
+                    pipe_segment.matrix[0][1] = "X"
           
           if down < len(pipe_map):
                pipe_check = pipe_map[down][pipe_segment.x]
                if pipe_check.type in ["|", "L", "J"]:
                     connections.append((down, pipe_segment.x))
+                    pipe_segment.matrix[2][1] = "X"
 
           if left >= 0:
                pipe_check = pipe_map[pipe_segment.y][left]
                if pipe_check.type in ["-", "L", "F"]:
                     connections.append((pipe_segment.y, left))
+                    pipe_segment.matrix[1][0] = "X"
 
           if right < len(pipe_map[0]):
                pipe_check = pipe_map[pipe_segment.y][right]
                if pipe_check.type in ["-", "J", "7"]:
                     connections.append((pipe_segment.y, right))
+                    pipe_segment.matrix[1][2] = "X"
+
 
           assert(len(connections) == 2)
           return connections[0], connections[1]
@@ -122,44 +155,83 @@ def walk_pipes(pipe_map:[[]], start_y:int , start_x:int ) -> int:
      return steps
 
 
-def flood_fill_outer(pipe_map = [[]]):
-     sys.setrecursionlimit(6000)
-     height = len(pipe_map)
-     width = len(pipe_map[0])
-     def fill(y, x):
-          if (y < 0) or (y >= height) or (x < 0) or (x >= width):
-               return
-          if pipe_map[y][x].step_number == EMPTY:
-               pipe_map[y][x].step_number = OUTSIDE
-               neighbors =  [(y-1,x),(y+1,x),(y,x-1),(y,x+1)]
-               for n in neighbors:
-                    fill(n[0],n[1])
+def flood_fill_outer(output_map = [[]]):
      
+     height = len(output_map)
+     width = len(output_map[0])
+     
+     def fill(y, x, fill_char = " ", check_char = "O"):
+          stack = [(y, x)]
+
+          while stack:
+               curr_y, curr_x = stack.pop()
+               
+               if (curr_y < 0) or (curr_y >= height) or (curr_x < 0) or (curr_x >= width):
+                    continue
+
+               cur = output_map[curr_y][curr_x]
+               if cur == check_char:
+                    output_map[curr_y][curr_x] = fill_char
+                    neighbors = [(curr_y - 1, curr_x), (curr_y + 1, curr_x), (curr_y, curr_x - 1), (curr_y, curr_x + 1)]
+                    stack.extend(neighbors)
+
+
      # Assume 0,0 is safe to start with
      fill(0, 0)
+     fill(0, 0, "O", " ")
+     fill(0, 0)
 
-     #known outside
-     fill(66,0)
-     fill(73,0)
 
+def clean_matrix(pipe_map = [[]]):
+     for row in pipe_map:
+          for pipe_segment in row:
+               if (pipe_segment.step_number == EMPTY): 
+                    pipe_segment.matrix = matrix_map["."]
+
+
+
+def construct_output_map(pipe_map = [[]]):
+     output_map = []
+     y_max = len(pipe_map) * 3
+     x_max = len(pipe_map[0])
+     for y in range(y_max):
+          output_map.append([])
+          for x in range(x_max):
+               y_item = y // 3
+               y_step = y % 3
+               pipe_segement = pipe_map[y_item][x]
+               matrix_cur = pipe_segement.matrix[y_step]
+               for item in matrix_cur:
+                    output_map[y].append(item)
+               
+          
+     return output_map
 
 pipe_map, start_y, start_x = extract_data(file_path)
 farthest_step = walk_pipes(pipe_map, start_y, start_x )
+clean_matrix(pipe_map)
 print("Part 1: ",farthest_step) #6820
 
-flood_fill_outer(pipe_map)
+output_map = construct_output_map(pipe_map)
+flood_fill_outer(output_map)
 #debug print map
 
 def print_output_map(base_dir, pipe_map):
+     count_o = 0
      output_path = os.path.join(base_dir, "output.txt")
      with open(output_path, 'w') as output_file:
-          for row in pipe_map:
-               for pipe_segment in row:
-                    output_file.write(pipe_segment.step_number)
+          for row in output_map:
+               for output_item in row:
+                    output_file.write(output_item)
+                    count_o += output_item.count('O')
                     output_file.flush()
                output_file.write('\n')
 
+     print("Part 2: ", count_o // 9) # 337
+
 print_output_map(base_dir, pipe_map)
+
+
 
 #part 2 
 # 201 is too low
