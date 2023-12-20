8f61d718b1b1683d41f151c011ca37f76fb77bca
Matt Hartfield <CounterTorque@users.noreply.github.com>
2023-12-20 08:48:43 -0600

    Day 20 parse out data

--- Day20/day20_solver.py
+++ Day20/day20_solver.py
@@ -1,4 +1,5 @@
 import os
+import re 
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "input.txt")
@@ -31,6 +32,14 @@ class ConjunctionModule:
           for input in inputs:
                self.stored_input_signal[input] = LOW
 
+def split_data (line) -> Tuple(str, [str]):
+     result = re.split(r'\s*->\s*', line)
+     name, connections = result[0].strip(), result[1]
+
+     conn_list = [con.strip() for con in connections.split(",")]
+
+     return name, conn_list
+
 
 def extract_data(file_path):
      broadcast_module = None
@@ -41,16 +50,27 @@ def extract_data(file_path):
           for line in file:
                if "broadcaster" in line:
                    #pull outputs and create class
-                   pass
+                   #broadcast -> a, b, c
+                   name, conn_list = split_data(line)
+                   assert(name == "broadcast")
+                   broadcast_module = BroadcastModule(conn_list)
+                   continue
               
                if "%" in line:
                    #pull name and connections and create class
-                   pass
+                   name, conn_list = split_data(line)
+                   name = name[1:]
+                   ff_mod = FlipFlopModule(name, conn_list)
+                   flipflop_modules.append(ff_mod)
+                   continue
               
                if "&" in line:
                    #pull name and connections and create class
-              
-                   pass
+                   name, conn_list = split_data(line)
+                   name = name[1:]
+                   con_mod = ConjunctionModule(name, conn_list)
+                   conjunction_modules.append(con_mod)
+                   continue
 
      
      for con_module in conjunction_modules:
