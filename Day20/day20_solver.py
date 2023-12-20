f57c75027507f567186084cd1d8fe1889e63c15c
Matt Hartfield <hartfield@gmail.com>
2023-12-20 08:00:14 -0600

    more work on basic wiring flow

--- Day20/day20_solver.py
+++ Day20/day20_solver.py
@@ -18,21 +18,48 @@ class FlipFlopModule:
           self.state = OFF
           self.name = name
           self.connections = connections
-          
+
 
 class ConjunctionModule:
-     def __init__(self, name: str, inputs:[str], connections:[str]):
+     def __init__(self, name: str, connections:[str]):
           self.name = name
           self.connections = connections
           self.stored_input_signal = {}
+
+     def setup_inputs(self, inputs:[str]):
+          assert(self.stored_input_signal.empty())
           for input in inputs:
                self.stored_input_signal[input] = LOW
 
 
 def extract_data(file_path):
-    with open(file_path, 'r') as file:
-         for line in file:
-              return
+     broadcast_module = None
+     flipflop_modules = {}
+     conjunction_modules = {}
+
+     with open(file_path, 'r') as file:
+          for line in file:
+               if "broadcaster" in line:
+                   #pull outputs and create class
+                   pass
+              
+               if "%" in line:
+                   #pull name and connections and create class
+                   pass
+              
+               if "&" in line:
+                   #pull name and connections and create class
+              
+                   pass
+
+     
+     for con_module in conjunction_modules:
+          
+          #have to afterwords wire up inputs
+          # could be broadcast, conjuction, or flipflop
+          pass
+
+     
          
 extract_data(file_path)
 
