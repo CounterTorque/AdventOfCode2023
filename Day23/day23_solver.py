348c16da415a1cf73a7196d2d93dc8b0a41e67bd
Matt Hartfield <hartfield@gmail.com>
2023-12-23 08:06:18 -0600

    Day23 Part 1 solved

--- Day23/day23_solver.py
+++ Day23/day23_solver.py
@@ -1,4 +1,5 @@
 import os
+import heapq
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "input.txt")
@@ -11,7 +12,14 @@ directions = {
      "E": (0, 1),
      "W": (0, -1)
 }
-slopes = {
+up_slopes = {
+     "N" : "v",
+     "S" : "^",
+     "E" : "<",
+     "W" : ">"
+     
+}
+down_slopes = {
      "^": "N",
      "v": "S",
      ">": "E",
@@ -28,7 +36,7 @@ def extract_data(file_path):
                hiking_map.append([])
                for x in range(len(char_list[y])):
                     char = char_list[y][x]  
-                    hiking_map[y].append(int(char))
+                    hiking_map[y].append(char)
 
      return hiking_map
           
@@ -48,13 +56,56 @@ def  find_start_and_end(heat_map):
 
 
 def walk_map(hiking_map, start, end):
-     #for each possible direction, check if we can move in that direction
-     #keep a list of the locations we have visited (so start with the start location)
-     #if we can move in more than one direction, make a duplicate branch. 
-     #if we ever deadend, kill the branch
-     #once we reach the end, return the distance
-     #take the highest distance
-     pass
+     
+     longest_path = 0
+     path_q = [[start]]
+
+     while path_q:
+          cur_path = heapq.heappop(path_q)
+
+          tail_pos = cur_path[-1]
+          if (tail_pos[0] == end[0]) and (tail_pos[1] == end[1]):
+               path_len = len(cur_path)
+               if path_len > longest_path:
+                    longest_path = path_len
+               continue
+
+          if hiking_map[tail_pos[0]][tail_pos[1]] != PATH:
+               slope_direction = down_slopes[hiking_map[tail_pos[0]][tail_pos[1]]]
+               new_pos = (tail_pos[0] + directions[slope_direction][0], tail_pos[1] + directions[slope_direction][1])
+               cur_path.append(new_pos)
+               heapq.heappush(path_q, cur_path)
+               continue
+
+          
+          for direction in directions:
+               new_pos = (tail_pos[0] + directions[direction][0], tail_pos[1] + directions[direction][1])
+               if new_pos[0] < 0 or new_pos[1] < 0 or new_pos[0] >= len(hiking_map) or new_pos[1] >= len(hiking_map[0]):
+                    continue
+
+               #todo handle not allowing onto path we've already taken
+               if new_pos in cur_path:
+                    continue
+
+               if hiking_map[new_pos[0]][new_pos[1]] == FOREST:
+                    continue
+
+               #check if we can move in that direction
+               if hiking_map[new_pos[0]][new_pos[1]] == PATH:
+                    new_path = cur_path[:]
+                    new_path.append(new_pos)
+                    heapq.heappush(path_q, new_path)
+                    continue
+
+               #check for slopes away from us
+               if hiking_map[new_pos[0]][new_pos[1]] == up_slopes[direction]:
+                    continue              
+
+               new_path = cur_path[:]
+               new_path.append(new_pos)
+               heapq.heappush(path_q, new_path)
+   
+     return longest_path - 1
 
 
 def print_map(map):
@@ -70,6 +121,6 @@ hiking_map = extract_data(file_path)
 start, end = find_start_and_end(hiking_map)
 path_length = walk_map(hiking_map, start, end)
 
-print(f"Part 1: {path_length}")
+print(f"Part 1: {path_length}") #2294
 
 
