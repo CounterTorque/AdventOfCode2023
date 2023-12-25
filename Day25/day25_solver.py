ebad90e1b15ba881bcde193eb32af973dbddc553
Matt Hartfield <hartfield@gmail.com>
2023-12-25 00:28:41 -0600

    Day 25 graph

--- Day25/day25_solver.py
+++ Day25/day25_solver.py
@@ -1,10 +1,11 @@
 import os
-from itertools import combinations
-import heapq
-import copy
+import sys
+import networkx as nx
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "input.txt")
+
+sys.setrecursionlimit(5000)
 
 def extract_data(file_path):
      components = {}
@@ -23,59 +24,25 @@ def extract_data(file_path):
                     components[conn].append(core_componenet)
      
      return components
-          
-
-def count_groups(components):
-
-     visited = set()
-     num_groups = 0
-     group_sizes = []
-
-     def dfs(component):
-         visited.add(component)
-         group_sizes[num_groups] += 1
-         for conn in components[component]:
-             if conn not in visited:
-                 dfs(conn)
-
-     for component in components:
-         if component not in visited:
-             group_sizes.append(0)
-             dfs(component)
-             num_groups += 1
-
-     return num_groups, group_sizes
 
 
 def split_components(components):
-     trio_q = list(combinations(components.keys(), 6))
-
-     max_size = 0
-     while trio_q:
-          conn_a1, conn_a2, conn_b1, conn_b2, conn_c1, conn_c2 = heapq.heappop(trio_q)
-          if (conn_a2 not in components[conn_a1] or
-               conn_b2 not in components[conn_b1] or 
-               conn_c2 not in components[conn_c1]):
-               continue
-         
-          exp_componenets = copy.deepcopy(components)
-          exp_componenets[conn_a1].remove(conn_a2)
-          exp_componenets[conn_a2].remove(conn_a1)
-          exp_componenets[conn_b1].remove(conn_b2)
-          exp_componenets[conn_b2].remove(conn_b1)
-          exp_componenets[conn_c1].remove(conn_c2)
-          exp_componenets[conn_c2].remove(conn_c1)
-
-          cur_groups, group_sizes = count_groups(exp_componenets)
-          if (cur_groups > 1):
-               assert(cur_groups == 2)
-               max_size = group_sizes[0] * group_sizes[1]
-
-     return max_size
-     
+     graph = nx.DiGraph()
+     for key, value in components.items():
+          for item in value:
+               graph.add_edge(key, item, capacity=1)
+               graph.add_edge(item, key, capacity=1)
+               
+     for x in components.keys():
+          for y in components.keys():
+               if x == y:
+                    continue
+               cuts, (L,R) = nx.minimum_cut(graph, x, y)
+               if (cuts == 3):
+                    return (len(L) * len(R))
      
          
 components = extract_data(file_path)
 max_size = split_components(components)
 
-print(f"Part 1: {max_size}")
+print(f"Part 1: {max_size}") # 598120
