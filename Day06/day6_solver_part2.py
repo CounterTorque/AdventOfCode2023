a632594c65e35903d4beb29fa0abfb9565cf852f
Matt Hartfield <hartfield@gmail.com>
2023-12-25 10:01:18 -0600

    Day6 resplit into two solution files

--- Day06/day6_solver_part2.py
+++ Day06/day6_solver_part2.py
@@ -6,13 +6,11 @@ class RaceData:
         self.Better_Starts = []
         self.calc_better_starts()
 
-
     def calc_better_starts(self):
         for time_start in range(0, self.Time):
             if self.distance_if(time_start) > self.Distance:
                 self.Better_Starts.append(time_start)
         return
-    
     
     def distance_if(self, time_start):
         dist_per_second = time_start
@@ -22,16 +20,8 @@ class RaceData:
     
     def record_ways_count(self):
         return len(self.Better_Starts)
-    
 
-race_data = [RaceData(55, 246),
-             RaceData(82, 1441),
-             RaceData(64, 1012),
-             RaceData(90, 1111),]
+race_data = [RaceData(55826490, 246144110121111)]
+solution_1 = (race_data[0].record_ways_count())
 
-solution_1 = (race_data[0].record_ways_count() * 
-                race_data[1].record_ways_count() * 
-                race_data[2].record_ways_count() *
-                race_data[3].record_ways_count())
-
-print(f"Part 1:{solution_1}") # 608902
+print(f"Part 2: {solution_1}") #46173809
